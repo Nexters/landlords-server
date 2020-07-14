7558c6b8b36d873e5e739a8a254e39f92508d7fe
Mincheol Kim <mcauto@naver.com>
2020-07-14 22:16:17 +0900

    feat(database): 데이터베이스 연동 및 테스트

--- src/core/config.py
+++ src/core/config.py
@@ -2,6 +2,25 @@
 from pydantic import BaseSettings
 
 
+class SQLAlchemySettings(BaseSettings):
+    MYSQL_USER: str = ""
+    MYSQL_PASSWORD: str = ""
+    MYSQL_ROOT_PASSWORD: str = ""
+    MYSQL_HOST: str = ""
+    MYSQL_DATABASE: str = ""
+
+    SQLALCHEMY_POOL_SIZE: int = 5
+    SQLALCHEMY_POOL_TIMEOUT: int = 10
+    SQLALCHEMY_POOL_RECYCLE: int = 3600
+    SQLALCHEMY_ECHO: bool = False
+    SQLALCHEMY_DATABASE_URL: str = "mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"  # noqa
+
+    class Config:
+        """ setting의 부가 설정 """
+
+        case_sensitive = True
+
+
 class Settings(BaseSettings):
     """
     application 설정 (환경변수 최우선)
@@ -16,3 +35,4 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+sqlalchemy_settings = SQLAlchemySettings()
