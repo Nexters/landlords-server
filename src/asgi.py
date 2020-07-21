1aaac132b0a0fd0b8ec46547cb04dc8890bbdb2c
박다예 <sju02030@gmail.com>
2020-07-22 02:20:49 +0900

    라우터 설정

--- src/asgi.py
+++ src/asgi.py
@@ -1,8 +1,17 @@
 """
 """
+from starlette.middleware.sessions import SessionMiddleware
+
+from .apps.auth.routes.oauth import router as oauth_router
+from .apps.auth.routes.token import router as token_router
 from .core.config import settings
+from .core.database import Base, engine
 from .main import create_app
 from .routes import api_v1
 
+Base.metadata.create_all(bind=engine)
 app = create_app()
 app.include_router(api_v1, prefix=f"{settings.API_VERSION_PREFIX}")
+app.add_middleware(SessionMiddleware, secret_key="!secret")
+app.include_router(oauth_router, prefix="/oauth", tags=["auth"])
+app.include_router(token_router, prefix="/token", tags=["auth"])
