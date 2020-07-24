085ebe38b3b49e9cc15095007f12a7de50a79eea
Mincheol Kim <mcauto@naver.com>
2020-07-25 01:48:03 +0900

    fix(rooms): 방 크롤링 예외처리 추가 (#19)
    * fix(rooms): 방 크롤링 예외처리 추가
    
    * fix(rooms): 예외처리 추가

--- tests/function/apps/rooms/test_services.py
+++ tests/function/apps/rooms/test_services.py
@@ -1,8 +1,6 @@
 from src.apps.rooms.models.domain.dabang import Dabang
-from src.apps.rooms.services import (
-    DabangCrawlingException,
-    get_dabang_room_detail,
-)
+from src.apps.rooms.services import get_dabang_room_detail
+from src.core.exceptions import CrawlingException
 
 
 def test_get_dabang() -> None:
@@ -10,7 +8,7 @@ def test_get_dabang() -> None:
         dabang: Dabang = get_dabang_room_detail(
             room_id="5f0681178839af708334481e"
         )
-    except DabangCrawlingException as err:
+    except CrawlingException as err:
         assert err
     else:
         print(dabang)
