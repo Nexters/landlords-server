913fa1e7b7dc79df94826197fc4da6c10d9bf6cf
Mincheol Kim <mcauto@naver.com>
2020-07-30 23:10:29 +0900

    feat(jwk): jwk 추가

--- src/apps/oauth/models.py
+++ src/apps/oauth/models.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
 
 from .entity import OAuthType
 
@@ -38,3 +40,23 @@ class UserInDB(UserInfo):
 
     class Config:
         orm_mode = True
+
+
+class JsonWebKey(BaseModel):
+    """ JWKS - [RFC 7571](https://tools.ietf.org/html/rfc7517) """
+
+    kty: str = Field(..., description="사용되는 값")
+    n: str = Field(..., description="RSA modulus")
+    e: str = Field(..., description="RSA public exponent")
+    kid: Optional[str] = Field(
+        default="ims-1",
+        description="jwt의 encode시 사용된 key의 고유 식별자 (for key rolling)",
+    )
+    alg: Optional[str] = Field(default="RSA256", description="사용된 알고리즘")
+    use: Optional[str] = Field(
+        default="sig", description="용도 (sig: signature, verify 검증)"
+    )
+
+
+class JsonWebKeyResponse(BaseModel):
+    keys: List[JsonWebKey]
--- src/apps/oauth/services.py
+++ src/apps/oauth/services.py
@@ -1,7 +1,8 @@
 import time
-from typing import Optional
+from typing import Generator, Optional
 
-import jwt
+from authlib.jose import jwk, jwt
+from cryptography.hazmat.backends.openssl.rsa import _RSAPublicKey
 from fastapi.param_functions import Depends
 from fastapi.security.api_key import APIKeyHeader
 from sqlalchemy.orm import Session
@@ -10,7 +11,17 @@ from ...core.config import settings
 from ...core.database import get_database_session
 from .entity import User
 from .exceptions import UserNotFound
-from .models import GoogleUserInfo, UserInDB, UserInfo
+from .models import GoogleUserInfo, JsonWebKey, UserInDB, UserInfo
+
+
+def get_jwk(key: str) -> JsonWebKey:
+    header = {"alg": "RS256", "kid": "landlords-1", "use": "sig"}
+    jwk_dict = jwk.dumps(key, **header, kty="RSA")
+    return JsonWebKey(**jwk_dict)
+
+
+def get_public_key() -> Generator[_RSAPublicKey, None, None]:
+    yield jwk.loads(get_jwk(settings.PUBLIC_KEY).dict())
 
 
 def create_access_token(
@@ -20,6 +31,7 @@ def create_access_token(
     """ 액세스 토큰 생성 (JWT) """
     iat = int(time.time())
     exp = iat + expiration
+    headers = {"alg": "RS256", "kid": "landlords-1"}
     jwt_body = google_user_info.dict(
         exclude={
             "iss",
@@ -32,14 +44,14 @@ def create_access_token(
         }
     )
     jwt_body.update({"iat": iat, "exp": exp})
-    token = jwt.encode(jwt_body, settings.PRIVATE_KEY, settings.JWT_ALGORITHM)
+    token = jwt.encode(
+        header=headers, payload=jwt_body, key=settings.PRIVATE_KEY
+    )
     return str(token, "utf-8")
 
 
-def decode_token(token: str) -> UserInfo:
-    payload = jwt.decode(
-        token, settings.PUBLIC_KEY, algorithms=settings.JWT_ALGORITHM
-    )
+def decode_token(token: str, public_key: _RSAPublicKey) -> UserInfo:
+    payload = jwt.decode(token, public_key)
     token_data = UserInfo(**payload)
     return token_data
 
@@ -50,8 +62,9 @@ AUTH_HEADER = APIKeyHeader(name="Authorization")
 async def get_current_user(
     token: str = Depends(AUTH_HEADER),
     session: Session = Depends(get_database_session),
+    public_key: _RSAPublicKey = Depends(get_public_key),
 ) -> UserInDB:
-    user_info = decode_token(token)
+    user_info = decode_token(token, public_key)
     user: Optional[User] = session.query(User).filter(
         User.email == user_info.email
     ).first()
