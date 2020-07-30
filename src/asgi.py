17666fdf7dbbb1fecf607f4b514e65aae87ca1fa
Kim Mincheol <nexters@kakao.com>
2020-07-30 23:10:29 +0900

    feat(jwk): jwk 추가

--- src/asgi.py
+++ src/asgi.py
@@ -5,6 +5,7 @@ from starlette.middleware.sessions import SessionMiddleware
 
 from . import version
 from .apps.oauth.client import oauth_client
+from .apps.oauth.routes import router as well_known_router
 from .core.config import settings
 from .core.database import Base, engine
 from .main import create_app
@@ -13,6 +14,7 @@ from .routes import api_v1
 Base.metadata.create_all(bind=engine)
 app = create_app()
 app.include_router(api_v1, prefix=f"{settings.API_VERSION_PREFIX}")
+app.include_router(well_known_router, prefix="/.well-known", tags=["auth"])
 
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 app.mount(path="/oauth", app=oauth_client, name="oauth")
