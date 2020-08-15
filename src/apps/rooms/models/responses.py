f7e8fe56b113cadd9552efa206ffb797bc542f55
Mincheol Kim <mcauto@naver.com>
2020-08-15 16:37:12 +0900

    fix(rooms): 크롤링 시 유저의 방 리스트에서 검사하도록 수정 (#44)
    docs(rooms): 도큐먼트 response 스키마 추가

--- src/apps/rooms/models/responses.py
+++ src/apps/rooms/models/responses.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from pydantic import BaseModel
-from starlette.responses import JSONResponse
 
 from .domain.landlords import RoomItemInDB
 
@@ -14,7 +13,3 @@ class RoomItemsResponse(BaseModel):
     """ 방 매물 정보 리스트 response """
 
     rooms: List[RoomItemInDB]
-
-
-class RoomItemHTTPResponse(JSONResponse):
-    """방 매물 정보 with HTTP Status"""
