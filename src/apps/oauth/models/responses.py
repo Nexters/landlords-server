b855b585b1e3a09a9f0f15bd1702d28899636312
Kim Mincheol <nexters@kakao.com>
2020-12-06 20:41:26 +0900

    feat(kakao-auto): 카카오 로그인 및 토큰 발급 추가 (#64)

--- src/apps/oauth/models/responses.py
+++ src/apps/oauth/models/responses.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Extra
 
+from .domain.kakao import KakaoAccount, Properties
 from .domain.landlords import JsonWebKey
 
 
@@ -11,3 +13,30 @@ class JsonWebKeyResponse(BaseModel):
 
 class AppTokenResponse(BaseModel):
     token: str
+
+
+class KakaoAuthResponse(BaseModel):
+    """ 카카오 인증 response
+        https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#request-token
+    """
+
+    access_token: str
+    token_type: str
+    refresh_token: str
+    expires_in: int
+    scope: str
+    refresh_token_expires_in: int
+
+
+class KakaoUserMeResponse(BaseModel):
+    """ 카카오 사용자 정보 가져오기
+        https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info
+    """
+
+    id: int
+    connected_at: datetime
+    properties: Properties
+    kakao_account: KakaoAccount
+
+    class Config:
+        extra = Extra.allow
--- src/main.py
+++ src/main.py
@@ -1,40 +1,13 @@
 """
 main.py
 """
-from authlib.common.errors import AuthlibBaseError
 from fastapi.applications import FastAPI
-from fastapi.exceptions import RequestValidationError
-from pydantic.error_wrappers import ValidationError
-from sqlalchemy.exc import SQLAlchemyError
-from starlette.exceptions import HTTPException
 
-from .core.exceptions import RepositoryException
-from .core.handlers import (
-    auth_exception_handler,
-    database_exception_handler,
-    http_exception_handler,
-    validation_exception_handler,
-)
+from .core.handlers import set_exception_handlers
 
 
 def create_app(title: str) -> FastAPI:
     """ app factory method """
     app = FastAPI(title=title)
-
-    app.add_exception_handler(
-        ValidationError, handler=validation_exception_handler
-    )
-
-    app.add_exception_handler(
-        RequestValidationError, handler=validation_exception_handler
-    )
-
-    app.add_exception_handler(
-        SQLAlchemyError, handler=database_exception_handler
-    )
-    app.add_exception_handler(
-        RepositoryException, handler=database_exception_handler
-    )
-    app.add_exception_handler(HTTPException, handler=http_exception_handler)
-    app.add_exception_handler(AuthlibBaseError, handler=auth_exception_handler)
+    set_exception_handlers(app)
     return app
